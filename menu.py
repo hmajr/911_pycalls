78cf1a491d7dee0e157b602bc496434112a1cfb4
Hélio Andrade <hmajr.tec@gmail.com>
2019-04-26 11:52:41 -0300

    Adciona except ValueError

--- menu.py
+++ menu.py
@@ -1,16 +1,17 @@
 import os
+from MyException import InvalidOption
 
 def menu_gui():
     #Limpa tela terminal
     os.system("cls" if os.name == 'nt' else 'clear')
 
-    print("------------------------------ MENU - -----------------------------")
+    print("------------------------------ 911 CALLS DATA -----------------------------")
     print("1. Top 5 zip codes for 911 calls")
     print("2. Top 5 townships for 911 calls")
     print("3. Count unique titles codes")
     print("4. Menu Option 4")
     print("5. Exit")
-    print("-------------------------------------------------------------------")
+    print("---------------------------------------------------------------------------")
 
     while True:
         try:
@@ -21,6 +22,8 @@ def menu_gui():
                 raise InvalidOption
         except InvalidOption as iopt:
             print("Tente outra opcao")
+        except ValueError as ve:
+            print(ve)
         else:
             if option == 1:
                 return "top_zips"
@@ -34,11 +37,3 @@ def menu_gui():
 
 if __name__ == "__main__":
     pass
-
-
-##
-##  CLASS
-##
-class InvalidOption(Exception):
-    def __ini__(self, message):
-        pass
