2770a5c78ff2d4fa8d83dfb93360b08538fa3909
Hélio Andrade <hmajr.tec@gmail.com>
2019-04-26 04:55:32 -0300

    Implementa try/exception leitura csv

--- main.py
+++ main.py
@@ -1,17 +1,54 @@
 if __name__ == "__main__":
+##
+## IMPORT
+##
+    ## LIBRARIES
     import numpy as np
     import pandas as pd
     from plotly.offline import download_plotlyjs, plot, iplot
     import cufflinks as cf
 
+    import menu
+
+    ## Interative plot offline
     cf.go_offline()
+## END - IMPORT
 
-    try:
-        df_911 = pd.read_csv("./dataset/911.csv")
-    except OSError as os:
-        print(os)
-    else:
-        print(df_911.info())
-        print("\n {}".format(df_911.head())
+##
+## CODE
+##
+    ## Importa CSV
+    df_911 = pd.read_csv("./dataset/911.csv")
+    # try:
+    #     df_911 = pd.read_csv("./dataset/911.csv")
+        
+    #     if pd.isnull(df_911):
+    #         raise OSError
+    # except Exception as ex:
+    #     print(ex)
+    # else:
+    #     print(df_911.info())
+    #     print("\n")
+    #     print(df_911.head())
 
+    print(df_911.info())
+    print("\n")
+    print(df_911.head())
     
+    ## LOOP OPCOES
+    running = True
+
+    while running:
+        option = menu.menu_gui()
+
+        if option == "top_zips":
+            pass
+        elif option == "top_towns":
+            pass
+        elif option == "unique_titles":
+            pass
+        elif option == "quit":
+            running = False
+        
+
+## END - CODE
