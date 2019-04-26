37bf70193afbe8c13478fa4f576d5f221a8f24f1
Hélio Andrade <hmajr.tec@gmail.com>
2019-04-26 12:36:45 -0300

    Adciona novas opcoes. Altera sys selecao itens

--- menu.py
+++ menu.py
@@ -4,21 +4,27 @@ from MyException import InvalidOption
 def menu_gui():
     #Limpa tela terminal
     os.system("cls" if os.name == 'nt' else 'clear')
-
+    option_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     print("------------------------------ 911 CALLS DATA -----------------------------")
     print("1. Top 5 zip codes for 911 calls")
     print("2. Top 5 townships for 911 calls")
     print("3. Count unique titles codes")
-    print("4. Menu Option 4")
-    print("5. Exit")
+    print("4. Most common reason for 911 calls")
+    print("5. Menu Option 4")
+    print("6. Menu Option 4")
+    print("7. Menu Option 4")
+    print("8. Menu Option 4")
+    print("9. Menu Option 4")
+    print("10. Exit")
     print("---------------------------------------------------------------------------")
 
     while True:
         try:
-            option = int(input("Enter your choice[1-5]:"))
-            print(option)
+            option = int(input("Enter your choice [{}-{}]:".format(option_list[0], option_list[-1])))
             
-            if (option < 1) or (option > 5):
+            if option in option_list:
+                print(option)
+            else:
                 raise InvalidOption
         except InvalidOption as iopt:
             print("Tente outra opcao")
@@ -31,7 +37,9 @@ def menu_gui():
                 return "top_towns"
             if option == 3:
                 return "unique_titles"
-            if option == 5:
+            if option == 4:
+                return "top_common-reasons"
+            if option == 10:
                 return "quit"
 
 
